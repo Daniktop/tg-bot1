696f139c1a17342adb958f3563c8c7600853713c
Daniktop <73233974+Daniktop@users.noreply.github.com>
2026-03-29 17:00:16 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
 import asyncio
 import os
-import random
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
 
-TOKEN = os.getenv("8498298692:AAEnYgsoJ4MhkSbsjdZiR5fFLXycz5J2l7o")
+TOKEN = os.getenv("8498298692:AAEnYgsoJ4MhkSbsjdZiR5fFLXycz5J2l7o")  # или вставь строкой
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Команда /start
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer("🚀 Бот работает на Render!")
+    await message.answer("👋 Привет! Я тестовый бот.")
+
+# Команда /balance
+@dp.message(Command("balance"))
+async def balance(message: Message):
+    await message.answer("💰 Твой баланс: 1000$")
 
 async def main():
+    print("Бот запущен...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
